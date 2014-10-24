3c6e3d42eb34e78bc0eb8e371cf2639539fa1309
Kevin Adams <kevinadams@Kevins-iMac.local>
2014-10-24 00:09:25 -0700

    modified index.html so that state class has a state id. modified signup.js so that it will load us-states.js and have it as an option in a drop down menu.

--- js/signup.js
+++ js/signup.js
@@ -2,3 +2,21 @@
     Signup Form Script
     This script will load the state select list and validate the form before submission
 */
+
+function onReady() {
+	var element = document.getElementById("state");
+	var name = document.createElement("name");
+
+	for (var i = 0; i < usStates.length; i++) {
+		var option = document.createElement("option");
+		name = usStates[i].name;
+		option.value = usStates[i].code;
+		var text = document.createTextNode(name);
+		option.appendChild(text);
+		element.appendChild(option);
+	}
+}
+
+
+
+document.addEventListener('DOMContentLoaded', onReady);
